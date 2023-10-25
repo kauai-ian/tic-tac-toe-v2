2a92a2f6d8476970c98951c6c2f3231c00341a12
Ian <ian@student.dev-island.org>
2023-10-25 06:45:02 -1000

    added a few comment questions for errors

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-let gameWinner = null;
-const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+
 
 // 1. add new players
 // factory function to create player.
@@ -27,93 +17,52 @@ function newPlayer(_name, _key) {
 (() => {
   const gameContainer = document.querySelector(".container");
   const gameboard = [...Array(9).keys()];
+  const boxes = [] // is this correct, to put the boxes in an array?
   console.log(gameContainer);
   gameboard.map((i) => {
     const box = document.createElement("div");
     box.classList.add("boxes");
-    console.log(box);
     box.dataset.index = i;
+    boxes.push(box) // add the box to the array
     // box.addEventListener("click", (e) => game(e));
     gameContainer.append(box);
   });
-})();
+  const status = document.querySelector(".status");
+// })();
 
-// module to run the game (create player)
-(() => {
+// // module to run the game (create player)
+// (() => {
   const player1 = newPlayer("player1", "x");
   const player2 = newPlayer("player2", "o");
   console.log(player1, player2);
   let currentPlayer = player1;
-  const gameContainer = document.querySelector(".container");
+  // const gameContainer = document.querySelector(".container");
+  // const box = document.getElementsByClassName(".boxes")
   gameContainer.addEventListener("click", (e) => {
-    e.target.innerHTML = currentPlayer.key;
-    changePlayer()
-  });
-  function changePlayer() {
-    if (currentPlayer.key == "x") {
-      currentPlayer = player2;
-    } else {
-      currentPlayer = player1;
+    if (!gameWinner && boxes.every(box => box.textContent !== "")) {// not working
+      status.textContent = "It's a tie game";
+      gameOver()
     }
-  }
-  // check for winner before change player
-  // check score
-  //
-})();
-
-// 2. game starts when user clicks on a space an x is given - module
-spaceElements.forEach((spaceElement) => {
-  spaceElement.element.addEventListener("click", (e) => {
-    console.log(`player chooses ${spaceElement.index}`);
-    game(e);
+    else if (!gameWinner && e.target.classList.contains("boxes") && e.target.textContent === "") {
+    e.target.innerHTML = currentPlayer.key;
+    checkWinner(currentPlayer, boxes)
+    changePlayer()
+    }
   });
-});
 
-// 4.
-const resetBtn = document.getElementById("resetBtn");
-resetBtn.addEventListener("click", () => {
-  spaceElements.content = "";
-  spaceElements.innerHTML = "";
-});
+  let gameWinner = null;
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-function game(e) {
-  console.log(e);
-  const playerChoice = spaceElements.find((obj) => obj.element === e.target);
-  if (currentPlayer === "x") {
-    playerChoice.content = "x";
-    playerChoice.element.innerHTML = "x";
-  }
-  if (checkWinner("x") || checkWinner("o")) {
-    gameOver();
-  } else if (currentPlayer === "x") {
-    currentPlayer = "o";
-    getComputerChoice();
-    checkWinner("o");
-    currentPlayer = "x";
-  }
-}
-// 6. computer selection - module
-function getComputerChoice() {
-  const availableSpaces = spaceElements.filter(
-    (spaceElement) => !spaceElement.content
-  );
-  if (!availableSpaces.length) {
-    console.log("Tie Game");
-    gameOver();
-    return;
-  }
-
-  const randomIndex = Math.floor(Math.random() * availableSpaces.length);
-  const randomChoice = availableSpaces[randomIndex];
-
-  randomChoice.content = "o";
-  randomChoice.element.innerHTML = "o";
-
-  console.log("Computer chooses", randomChoice.index);
-}
-
-// 3. for each space check if the index matches array of winning indexes. If it does not, then drop out. If all values of this are true, then return winner.
-function checkWinner(player) {
+  function checkWinner(player, boxes) {
   let foundWinner = false;
   winningCombos.forEach((combo) => {
     let comboIsWinner = true;
@@ -122,7 +71,7 @@ function checkWinner(player) {
     }
     for (let i = 0; i < combo.length; i++) {
       const index = combo[i];
-      if (spaceElements[index].content !== player) {
+      if (boxes[index].textContent !== player.key) {
         comboIsWinner = false;
         break;
       }
@@ -130,20 +79,61 @@ function checkWinner(player) {
     if (comboIsWinner) {
       foundWinner = true;
       combo.forEach((index) => {
-        spaceElements[index].element.classList.add("winner");
+       boxes[index].classList.add("winner");
       });
     }
   });
   if (foundWinner === true) {
     gameWinner = player;
-    console.log(gameWinner, "is winner");
+    status.textContent = `${player.name} is winner`;
     gameOver();
   }
 }
 
 function gameOver() {
-  console.log("game over");
   resetBtn.classList.toggle("hidden");
 }
 
-// 5. change display status
+function changePlayer() {
+    if (currentPlayer.key == "x") {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    } 
+  }
+
+  // check score
+
+
+  // 4. reset
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.textContent = "";
+    box.classList.remove("winner")
+  }) 
+  gameWinner = null
+  status.textContent = "Let's Play"
+  resetBtn.classList.add("hidden")
+});
+})();
+
+
+
+
+// function game(e) {
+//   console.log(e);
+//   const playerChoice = spaceElements.find((obj) => obj.element === e.target);
+//   if (currentPlayer === "x") {
+//     playerChoice.content = "x";
+//     playerChoice.element.innerHTML = "x";
+//   }
+//   if (checkWinner("x") || checkWinner("o")) {
+//     gameOver();
+//   } else if (currentPlayer === "x") {
+//     currentPlayer = "o";
+//     getComputerChoice();
+//     checkWinner("o");
+//     currentPlayer = "x";
+//   }
+// }
